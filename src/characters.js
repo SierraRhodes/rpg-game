128f3fcb9b143f35a35060ae81c234dfcdab767b
lilbitsnpieces <alreadywrote@gmail.com>
2023-03-06 20:07:02 -0800

    Add character archetype test passed

--- src/characters.js
+++ src/characters.js
@@ -4,6 +4,31 @@ export default class Characters {
     this.health = health;
     this.attackPower = attackPower;
     this.archetype = archetype;
-  }
 
+    if(archetype === "mage") {
+      this.health = 150;
+      this.attackPower = 200; 
+    }else if(archetype === "warrior"){
+      this.health = 300;
+      this.attackPower = 500;
+    }else if(archetype === "wizard"){
+      this.health = 100;
+      this.attackPower = 250;
+    } else if(archetype === "rogue"){
+      this.health = 200;
+      this.attackPower = 300; 
+    }
+  }
+  getArchetypeCategory() {
+    if(this.archetype === "mage") {
+      return "Magic user";
+    }else if(this.archetype === "warrior") {
+      return "Melee user";
+    }else if(this.archetype === "wizard") {
+      return "Magic user";
+    } else if(this.archetype === "rogue") {
+      return "Stealth user";
+    } else
+     return "Unrecognized archetype";
+}
 }
