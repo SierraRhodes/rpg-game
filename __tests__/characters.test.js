02eac3dfd186b559a5176f57580fa03bb4583159
lilbitsnpieces <alreadywrote@gmail.com>
2023-03-06 20:06:32 -0800

    Add character archetype test passed

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -3,18 +3,51 @@ import Characters from './../src/characters.js';
 describe('Character', () => {
 
   test('should correctly construct a character object with four character archtypes', () => {
-
-    const name = "Wizard";
+    const name = "Wiz";
     const health = 50;
-    const attackPower = 200;
-    const archetype = "Magic";
+    const attackPower = 50;
+    const archetype = "Wizard";
 
     const characters = new Characters(name, health, attackPower, archetype);
-
-    
-    expect(characters.name).toEqual("Wizard");
+    expect(characters.name).toEqual("Wiz");
     expect(characters.health).toEqual(50);
-    expect(characters.attackPower).toEqual(200);
-    expect(characters.archetype).toEqual("Magic");
+    expect(characters.attackPower).toEqual(50);
+    expect(characters.archetype).toEqual("Wizard");
   });
+
+  test('should correctly get archetypes health and attack power', () => {
+    const wizard = new Characters("Wiz", 100, 250, "wizard");
+    expect(wizard.health).toEqual(100);
+    expect(wizard.attackPower).toEqual(250);
+
+    const warrior = new Characters("Wren", 300, 500, "warrior");
+    expect(warrior.health).toEqual(300);
+    expect(warrior.attackPower).toEqual(500);
+
+    const mage = new Characters("Magi", 150, 200, "mage");
+    expect(mage.health).toEqual(150);
+    expect(mage.attackPower).toEqual(200);
+
+    const rogue = new Characters("Rouge", 200, 300, "rogue");
+    expect(rogue.health).toEqual(200);
+    expect(rogue.attackPower).toEqual(300);
+  })
+
+  test('should categorize archetypes', () => {
+    const wizard = new Characters("Wiz", 100, 250, "wizard");
+    expect(wizard.getArchetypeCategory()).toEqual("Magic user");
+
+    const warrior = new Characters("Wren", 300, 500, "warrior");
+    expect(warrior.getArchetypeCategory()).toEqual("Melee user");
+
+    const mage = new Characters("Magi", 150, 200, "mage");
+    expect(mage.getArchetypeCategory()).toEqual("Magic user");
+
+    const rogue = new Characters("Rouge", 200, 300, "rogue");
+    expect(rogue.getArchetypeCategory()).toEqual("Stealth user");
+  }); 
+  test('should return "Unrecognized archetype" for unknown archetype', () => {
+    const characters = new Characters("Will", 400, 300, "ninja");
+    expect(characters.getArchetypeCategory()).toEqual("Unrecognized archetype");
+  });  
 });
